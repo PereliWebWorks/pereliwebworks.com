c3cfbc08a1e38b50c89899ce83eb77081c3ba24c
Andrew Pereli <drewpereli@gmail.com>
2017-03-19 02:39:58 -0500

    added fonts and new animations

--- public_html/index.php
+++ public_html/index.php
@@ -5,6 +5,8 @@
   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
   crossorigin="anonymous"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/plugins/CSSPlugin.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/plugins/ScrollToPlugin.min.js"></script>
   	<script src="library/velocity.js"></script>
 	<script src="library/ScrollMagic/ScrollMagic.js"></script>
 	<script src="library/ScrollMagic/plugins/animation.gsap.js"></script>
@@ -17,45 +19,80 @@
 </head>
 <body>
 	<div id="container">
-		<nav id="navbar">abcdsasd</nav>
-		<div class="panel" id="panel_1" data-p-num="1"></div>
-		<div class="panel" id="panel_2" data-p-num="2"></div>
-		<div class="panel" id="panel_3" data-p-num="3"></div>
+		<nav id="navbar">
+			<a href="#container">Home</a>
+			<a href="#panel-2-anchor">Panel 2</a>
+		</nav>
+		<div class="panel" id="panel_1" data-p-num="1">
+			<img src="https://static.pexels.com/photos/25926/pexels-photo-25926.jpg"/>
+		</div>
+		<div class="panel" id="panel_2" data-p-num="2">
+			<img src="https://static.pexels.com/photos/39811/pexels-photo-39811.jpeg"/>
+		</div>
+		<div class="panel" id="panel_3" data-p-num="3">
+			<img src="https://static.pexels.com/photos/33688/delicate-arch-night-stars-landscape.jpg"></div>
+		</div>
 		<!-- This div is where all of the triggers and text are -->
 		<div id="layout-container">
 			<div class="panel-layout" id="panel-1-layout" data-layout-for="panel_1">
+				<div class="start-scroll-trigger"></div>
+				<h1 id="main-header">Pereli Web Works</h1>
+				<div class="large-spacer"></div>
+				<div id="main-header-disappear-trigger"></div>
 				<div class="spacer"></div>
 				<div class="animate-container">
 					<div class="animate-trigger"></div>
-					<h1 class="animate-text">Animate 1</h1>
+					<h2 class="animate-text">Animate 1</h1>
 				</div>
 				<div class="large-spacer"></div>
 				<div class="animate-container">
 					<div class="animate-trigger"></div>
-					<h1 class="animate-text">Animate 2</h1>
+					<h2 class="animate-text">Animate 2</h1>
 				</div>
 				<div class="large-spacer"></div>
 				<div class="animate-container">
 					<div class="animate-trigger"></div>
-					<h1 class="animate-text">Animate 3</h1>
+					<h2 class="animate-text">Animate 3</h1>
+				</div>
+				<div class="large-spacer"></div>
+				<div id="site-example-trigger"></div>
+				<div id="site-examples">
+						<div>
+							<span class="site-example">1</span>
+							<span class="site-example">2</span>
+							<span class="site-example">3</span>
+						</div>
+						<div>
+							<span class="site-example">4</span>
+							<span class="site-example">5</span>
+							<span class="site-example">6</span>
+						</div>
+						<div>
+							<span class="site-example">7</span>
+							<span class="site-example">8</span>
+							<span class="site-example">9</span>
+						</div>
 				</div>
 				<div class="large-spacer"></div>
 				<div class="animate-container">
 					<div class="animate-trigger"></div>
-					<h1 class="animate-text">Animate 4</h1>
+					<h2 class="animate-text">Animate 4</h1>
 				</div>
 				<div class="large-spacer"></div>
 				<div class="animate-container">
 					<div class="animate-trigger"></div>
-					<h1 class="animate-text">Animate 5</h1>
+					<h2 class="animate-text">Animate 5</h1>
 				</div>
 				<div class="large-spacer"></div>
 				<div id="panel-wipe-trigger-2" data-triggers="2" class="panel-wipe-trigger"></div>
+				<div class="end-scroll-trigger"></div>
 			</div>
 			<div class="panel-layout" id="panel-2-layout" data-layout-for="panel_2">
+				<div class="start-scroll-trigger"></div>
 				<div class="spacer"></div>
 				<div class="spacer"></div>
 				<div class="spacer"></div>
+				<div id="panel-2-anchor"></div>
 				<div class="animate-container">
 					<div class="animate-trigger"></div>
 					<h1 class="animate-text">Animate 1</h1>
@@ -82,8 +119,10 @@
 				</div>
 				<div class="medium-spacer"></div>
 				<div id="panel-wipe-trigger-3" data-triggers="3" class="panel-wipe-trigger"></div>
+				<div class="end-scroll-trigger"></div>
 			</div>
 			<div class="panel-layout" id="panel-3-layout" data-layout-for="panel_3">
+				<div class="start-scroll-trigger"></div>
 				<div class="spacer"></div>
 				<div class="animate-container">
 					<div class="animate-trigger"></div>
@@ -110,6 +149,7 @@
 					<h1 class="animate-text">Animate 5</h1>
 				</div>
 				<div class="medium-spacer"></div>
+				<div class="end-scroll-trigger"></div>
 			</div>
 		</div>
 	</div>
--- public_html/js/animations.js
+++ public_html/js/animations.js
@@ -11,86 +11,100 @@ $(()=>{
 			)];
 	//console.log(panelOffsets);
 	//Create wipe animation
+	//Also add scroll animation
 	var currentPanel = 1; //Current panel we're looking at. Doesn't change until next panel is fully faded in
 	$(".panel").each((i, e) => {
 		var id = $(e).data("p-num");
-		if (i === 0)
-			return true;
-		var fromX;
-		var fromBSX;
-		if (i % 2 === 1){
-			fromX = "-100%";
-			fromBSX = "50px";
+		$layout = $("#panel-" + id + "-layout");
+		if (i !== 0){ //Don't do swipe animation for first panel
+			var fromX;
+			var fromBSX;
+			if (i % 2 === 1){
+				fromX = "-100%";
+				toX = "0%";
+				fromBSX = "50px";
+			}
+			else{
+				fromX = "0%";
+				toX = "-50%";
+				fromBSX = "-50px"
+			}
+			var wipeAnimation = new TimelineMax().fromTo(e, 1, 
+							{x: fromX, opacity: 0 /*boxShadow: fromBSX + " -50px 10px 5px rgba(0,0,0,.3);"*/}, 
+							{x: toX, opacity: 1, /*boxShadow: "0px 0px 10px 5px rgba(0,0,0,.3);",*/ ease: Power1.easeOut});
+			var wipeScene = new ScrollMagic.Scene({
+					triggerElement: $("#panel-wipe-trigger-" + id),
+					triggerHook: "onLeave",
+					duration: swipeDuration
+				})
+				.setTween(wipeAnimation)
+				.addTo(controller);
+			/*
+				These events help control the paralx effect.
+				The parallax scroll event listener checks to see which panel is the "current panel",
+				and only applies the effect to that panel.
+				During the wipe animation, there is no current panel.
+				After the wipe animation completes, the current panel is set to whichever panel is now visible.
+			*/
+			wipeScene.on("start", (event)=>{
+				if (event.scrollDirection === "FORWARD"){
+					currentPanel = false;
+					return;
+				}
+				var triggered = $(event.target.triggerElement()).data("triggers") - 1; //the panel triggered by the event
+				currentPanel = triggered;
+			});
+			wipeScene.on("end", (event)=>{
+				if (event.scrollDirection === "REVERSE"){
+					currentPanel = false;
+					return;
+				}
+				var triggered = $(event.target.triggerElement()).data("triggers"); //the panel triggered by the event
+				currentPanel = triggered;
+			});
+		}
+		var toLeft1;
+		var toLeft2;
+		if (i % 2 === 0){
+			toLeft1 = "-=200px";
+			toLeft2 = "+=200px";
 		}
 		else{
-			fromX = "100%";
-			fromBSX = "-50px"
+			toLeft1 = "+=200px";
+			toLeft2 = "-=200px";
 		}
-		var wipeAnimation = new TimelineMax().fromTo(e, 1, 
-						{x: fromX, opacity: "0" /*boxShadow: fromBSX + " -50px 10px 5px rgba(0,0,0,.3);"*/}, 
-						{x: "0%", opacity: "1", /*boxShadow: "0px 0px 10px 5px rgba(0,0,0,.3);",*/ ease: Power1.easeOut});
-		var wipeScene = new ScrollMagic.Scene({
-				triggerElement: $("#panel-wipe-trigger-" + id),
-				triggerHook: "onLeave",
-				duration: swipeDuration
-			})
-			.setTween(wipeAnimation)
-			.addTo(controller);
-		/*
-			These events help control the paralx effect.
-			The parallax scroll event listener checks to see which panel is the "current panel",
-			and only applies the effect to that panel.
-			During the wipe animation, there is no current panel.
-			After the wipe animation completes, the current panel is set to whichever panel is now visible.
-		*/
-		wipeScene.on("start", (event)=>{
-			if (event.scrollDirection === "FORWARD"){
-				currentPanel = false;
-				return;
-			}
-			var triggered = $(event.target.triggerElement()).data("triggers") - 1; //the panel triggered by the event
-			currentPanel = triggered;
-		});
-		wipeScene.on("end", (event)=>{
-			if (event.scrollDirection === "REVERSE"){
-				currentPanel = false;
-				return;
-			}
-			var triggered = $(event.target.triggerElement()).data("triggers"); //the panel triggered by the event
-			currentPanel = triggered;
-		});
+		//Add scroll effects
+		var animation = new TimelineMax()
+					.to($(e).find("img"), 2, {top: "-=500px", left: toLeft1, ease:Power1.easeOut})
+					.to($(e).find("img"), 2, {top: "-=500px", left: toLeft2, ease:Power1.easeIn});
+		new ScrollMagic.Scene({
+			triggerElement: $layout.find(".start-scroll-trigger"),
+			triggerHook: "onLeave",
+			duration: $layout.find(".end-scroll-trigger").offset().top - $layout.find(".start-scroll-trigger").offset().top
+		})
+		.setTween(animation)
+		.addTo(controller);
 	});
-	
-	//Create background image motion animation
-	/*
-	var animation = new TimelineMax()
-			.to("#panel_1", 1, {backgroundPositionY: "100px", ease: Linear.easeNone})
 
-	new ScrollMagic.Scene({
-						triggerElement: "#panel_1",
-						triggerHook: "onLeave",
-						duration: 2400
-					})
-					.setTween(animation)
-					.addIndicators({name: "1"})
-					.addTo(controller);
-	*/
-	var lastScrollPos = 0;
-	var rate = 10;
-	$(document).scroll((e)=>{
-		if (!currentPanel)
-			return;
-		$p = $("#panel_" + currentPanel);
-		var offset = panelOffsets[currentPanel-1];
-		yOffset = -($(document).scrollTop() - offset) / 8;
-		xOffset = -($(document).scrollTop() - offset) / 20;
-		if (yOffset > 0)
-			yOffset = 0;
-		if (xOffset > 0)
-			xOffset = 0;
-		$p.css("background-position-y", yOffset + "px");
-		$p.css("background-position-x", xOffset + "px");
-	})
+
+
+
+	//Animate example sites
+	var animation = new TimelineMax()
+		.staggerFromTo($(".site-example"), 1, 
+					{left: "0", top: "100vH", opacity: 0},
+					{left: "40%", top: "50vH", opacity: 1, ease: Elastic.easeOut.config(1, .75)},
+				.2)
+		.staggerTo($(".site-example"), 1, 
+					{left: "100%", top: "100vH", opacity: 0, ease: Back.easeIn.config(1), delay: 4},
+				.2);
+		new ScrollMagic.Scene({
+				triggerElement: $("#site-example-trigger"),
+				triggerHook: "onEnter",
+				duration: 2000
+			})
+			.setTween(animation)
+			.addTo(controller);
 	
 
 
@@ -117,9 +131,41 @@ $(()=>{
 
 	});
 	
+
+	//Animate header
+	var animation = new TimelineMax().to($("#main-header"), 1, {opacity: 0});
+	new ScrollMagic.Scene({
+		triggerElement: $("#main-header-disappear-trigger"),
+		triggerHook: "onEnter",
+		duration: 1000
+	})
+	.setTween(animation)
+	.addTo(controller);
 	
 	// build scene
 });
+
+//Add nice scrolling
+	// change behaviour of controller to animate scroll instead of jump
+	controller.scrollTo(function (newpos) {
+		TweenMax.to(window, 2, {scrollTo: {y: newpos}});
+	});
+
+	//  bind scroll to anchor links
+	$(document).on("click", "a[href^='#']", function (e) {
+		var id = $(this).attr("href");
+		if ($(id).length > 0) {
+			e.preventDefault();
+
+			// trigger scroll
+			controller.scrollTo(id);
+
+				// if supported by the browser we can even update the URL.
+			if (window.history && window.history.pushState) {
+				history.pushState("", document.title, id);
+			}
+		}
+	});
 
 
 
